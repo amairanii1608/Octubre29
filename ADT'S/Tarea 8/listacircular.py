411585720c9672fc1a395129c48c0e160ff1c244
amairanii1608 <60655527+amairanii1608@users.noreply.github.com>
2020-12-01 01:50:19 -0600

    Update listacircular.py

--- ADT'S/Tarea 8/listacircular.py
+++ ADT'S/Tarea 8/listacircular.py
@@ -5,7 +5,7 @@ class NodoCircular:
 
 class CircularList:
     def __init__ ( self):
-        self.__head = None
+        self.__ref = None
         self.__tail = None
         self.__size = 0
 
@@ -13,57 +13,57 @@ class CircularList:
         return self.__size
 
     def is_empty( self):
-        return self.__head==None
+        return self.__ref==None
 
     def preppend( self, value ):
         if self.is_empty():
-            self.__head = self.__tail = NodoCircular(value)
-            self.__tail.next = self.__head
+            self.__ref = self.__tail = NodoCircular(value)
+            self.__tail.next = self.__ref
         else:
             curr_node = NodoCircular(value)
-            curr_node.next = self.__head
+            curr_node.next = self.__ref
             self.__tail = curr_node
-            self.__tail.next = self.__head
+            self.__tail.next = self.__ref
 
     def append(self, value):
         if self.is_empty():
-           self.__head = self.__tail = NodoCircular(value)
-           self.__tail.next = self.__head
+           self.__ref = self.__tail = NodoCircular(value)
+           self.__tail.next = self.__ref
         else:
             curr_node = self.__tail
             self.__tail = curr_node.next = NodoCircular(value)
-            self.__tail.next = self.__head
+            self.__tail.next = self.__ref
 
-    def remove_from_head(self):
+    def remove_from_ref(self):
         if self.is_empty():
             print("La lista esta vacia")
-        elif self.__head == self.__tail:
-            self.__head = self.__tail = None
+        elif self.__ref == self.__tail:
+            self.__ref= self.__tail = None
         else:
-            self.__head = self.__head.next
-            self.__tail.next = self.__head
+            self.__ref = self.__head.next
+            self.__tail.next = self.__ref
 
     def remove_from_tail(self):
         if self.is_empty():
             print("La lista esta vacia")
-        elif self.__head == self.__tail:
-            self.__head = self.__tail = None
+        elif self.__ref == self.__tail:
+            self.__ref = self.__tail = None
         else:
-            curr_node = self.__head
+            curr_node = self.__ref
             while curr_node.next != self.__tail:
                 curr_node = curr_node.next
-            curr_node.next = self.__head
+            curr_node.next = self.__ref
             self.__tail = curr_node
 
     def transversal( self ):
-        curr_node = self.__head
-        while curr_node.next != self.__head:
+        curr_node = self.__ref
+        while curr_node.next != self.__ref:
             print(curr_node.dato)
             curr_node = curr_node.next
         print(curr_node.dato)
 
     def search( self , value ):
-        curr_node = self.__head
-        while curr_node.next != self.__head and value != curr_node.dato:
+        curr_node = self.__ref
+        while curr_node.next != self.__ref and value != curr_node.dato:
             curr_node = curr_node.next
         return print(value == curr_node.dato)
