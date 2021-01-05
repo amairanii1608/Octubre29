89c5ca1133d3962d7217e21216cda3f1a204cd96
amairanii1608 <60655527+amairanii1608@users.noreply.github.com>
2021-01-05 13:42:34 -0600

    Add files via upload

---
+++ ADT'S/05ENE21/pruebas_colas.py
@@ -0,0 +1,21 @@
+from colas import Queue
+
+q1= Queue()
+q1.enqueue(3)
+q1.enqueue(33)
+q1.enqueue(23)
+print(q1.to_string())
+
+print ("Prueba 2 de Queue")
+c1={"id": 1,"nombre": "Mario", "balance": 20.5}
+c2={"id": 2,"nombre": "Diana", "balance": 3456.5}
+c3={"id": 3,"nombre": "Bartolo", "balance": 1000000.0}
+
+atencion= Queue()
+atencion.enqueue(c1)
+atencion.enqueue(c2)
+atencion.enqueue(c3)
+print(atencion.to_string())
+siguiente= atencion.dequeue()
+print(f"Bienvenido Sr.{siguiente['nombre']}, ¿En que podemos servirle el dia de hoy?")
+print(atencion.to_string())
