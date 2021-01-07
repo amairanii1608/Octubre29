5bf08c5732fd2d71763b58066657cb55f243cf28
amairanii1608 <60655527+amairanii1608@users.noreply.github.com>
2021-01-07 13:11:33 -0600

    Add files via upload

---
+++ ADT'S/07ENE21/pruebas_colas.py
@@ -0,0 +1,34 @@
+from colas import Queue
+
+q1= Queue()
+q1.enqueue(3)
+q1.enqueue(33)
+q1.enqueue(23)
+print(q1.to_string())
+
+print ("Prueba 2 de Queue")
+c1={"id": 1,"nombre": "Mario", "balance": 20.5}
+c2={"id": 2,"nombre": "Diana", "balance": 3456.5}
+c3={"id": 3,"nombre": "Bartolo", "balance": 1000000.0}
+
+atencion= Queue()
+atencion.enqueue(c1)
+atencion.enqueue(c2)
+atencion.enqueue(c3)
+print(atencion.to_string())
+siguiente= atencion.dequeue()
+print(f"Bienvenido Sr.{siguiente['nombre']}, ¿En que podemos servirle el dia de hoy?")
+print(atencion.to_string())
+
+print("Pruebas de las colas con prioridad acotada")
+maestres = {"prioridad":4 , "descripción":"Maestres","personas":["juan P","diego H"]}
+niños = {"prioridad":2 , "descripción":"Niños","personas":["Santi H","Ángel H"]}
+mecanicos = {"prioridad":4 , "descripción":"Mecánicos","personas":["Diana T","Maria Z"]}
+
+cpa = BoundedPriorityQueue(7)
+cpa.enqueue(maestres['prioridad'], maestres)
+cpa.enqueue(niños['prioridad'], niños)
+cpa.enqueue(mecanicos['prioridad'], mecanicos)
+cpa.to_string()
+sig= cpa.dequeue()
+print(sig)
