f77409b7c437e3260c602ed4a9eb0a34537bfe82
amairanii1608 <60655527+amairanii1608@users.noreply.github.com>
2021-01-12 15:48:24 -0600

    Update Recursividad.py

--- ADT'S/12ENE_Recursiva/Recursividad.py
+++ ADT'S/12ENE_Recursiva/Recursividad.py
@@ -1,35 +1,31 @@
-def factorial(n):
+def factorial( n ):
     if n == 0:
         return 1
     else:
-        return factorial (n-1) * n
+        return factorial( n - 1 ) * n
 
-def printRev (n):
+def printRev( n ): # 3
     if n > 0:
         printRev( n - 1 )
         print(n)
-        
-def fibonacci(n):
+
+def fibonacci( n ):
     if n == 1 or n == 0:
         return n
     if n > 1:
-        return fibonacci(n-1) + fibonacci(n-2)
-        
+        return ( fibonacci(n-1) + fibonacci(n-2) )
+
 def main():
     for num in range(1,11,1):
         r = factorial(num)
-        print(f"El factorial de {num} es {r}")
+        print(f"El factorial de { num } es { r } ")
 
 def main2():
-    printRev(3)
+    printRev( 3 )
 
 def main3():
     for num in range(11):
         print(str(fibonacci(num)) + "," , end="")
     print("")
 
-main()
-
-main2()
- 
 main3()
