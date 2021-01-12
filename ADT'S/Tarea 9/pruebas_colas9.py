efd5aab3c33df6c578eab76ba63803f2b72d0d95
amairanii1608 <60655527+amairanii1608@users.noreply.github.com>
2021-01-12 10:02:20 -0600

    Update pruebas_colas9.py

--- ADT'S/Tarea 9/pruebas_colas9.py
+++ ADT'S/Tarea 9/pruebas_colas9.py
@@ -2,13 +2,14 @@ from colas import Queue,PriorityQueue
 
 print("Pruebas de las colas con prioridad no acotada")
 
-cpna = PriorityQueue()
-print(f"La cola esta vacia?  {pq.is_empty()}")
-print(f"el tamaño de la cola: {pq.length()}")
+cpna = PriorityQueue(7)
+print(f"La cola esta vacia?  {cpna.is_empty()}")
+print(f"el tamaño de la cola: {cpna.length()}")
 cpna.enqueue(5)
 cpna.enqueue(15)
 cpna.enqueue(10)
 cpna.enqueue(1)
+cpna.dequeue()
 print(cpna.to_string())
 while not cpna.is_empty():
         print(cpna.dequeue())
