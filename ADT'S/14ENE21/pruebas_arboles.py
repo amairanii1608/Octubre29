50c327592bdf5d42a4be34d2486dbe554a73231c
amairanii1608 <60655527+amairanii1608@users.noreply.github.com>
2021-01-14 22:24:44 -0600

    Update pruebas_arboles.py

--- ADT'S/14ENE21/pruebas_arboles.py
+++ ADT'S/14ENE21/pruebas_arboles.py
@@ -1,19 +1,18 @@
 class NodoArbol:
-    def __init__(self , value , left = None , rigth = None):
+    def __init__(self, value, left=None, right=None):
         self.data = value
-        self.rigth = rigth
         self.left = left
+        self.right = right
 
-arbol = NodoArbol("R" , NodoArbol("C") , NodoArbol("H"))
-
-print(arbol.left.data)
-
+arbol = NodoArbol("R", NodoArbol("C",NodoArbol("D"),None),NodoArbol("H",None,None))
 print(arbol.data)
+print(arbol.left.data)
+print(arbol.right.data)
+print(arbol.left.left.data)
 
-arbol2 = NodoArbol(4 , NodoArbol(3 , NodoArbol(2 , NodoArbol(2))) , NodoArbol(5))
-
+arbol2 = NodoArbol(4, NodoArbol(3,NodoArbol(2,NodoArbol(2))),NodoArbol(5,None,None))
 print(arbol2.data)
+print(arbol2.right.data)
 print(arbol2.left.data)
 print(arbol2.left.left.data)
 print(arbol2.left.left.left.data)
-print(arbol2.rigth.data)
